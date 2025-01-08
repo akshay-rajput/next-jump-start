476b9a88aa0102dbde93729e9f69ff7842ff19de
Akshay <akshay@olvy.co>
2025-01-08 16:59:13 +0530

    add - boilerplate homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,100 +1,133 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const contentsMap = [
+    {
+      link: "https://nextjs.org/",
+      title: "🚀 Next.js v15",
+      description:
+        "The latest version with app directory and server components.",
+    },
+    {
+      link: "https://tailwindcss.com/",
+      title: "🎨 Tailwind CSS",
+      description: "A utility-first CSS framework for rapid styling.",
+    },
+    {
+      link: "https://ui.shadcn.com/docs",
+      title: "🧩 shadcn/ui",
+      description: "Prebuilt, accessible, and customizable UI components.",
+    },
+    {
+      link: "https://www.typescriptlang.org/",
+      title: "⚙️ TypeScript",
+      description: "Strongly typed language for maintainable codebases.",
+    },
+    {
+      link: "https://typicode.github.io/husky/",
+      title: "🛠️ Pre-Commit Hooks",
+      description:
+        "Husky pre-commit hook for formatting staged files using Prettier.",
+    },
+  ];
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+  return (
+    <div className="main-container flex flex-col min-h-screen">
+      {/* boilerplate navbar */}
+      <nav className="navbar w-full sticky top-0 backdrop-blur-md p-4 ">
+        <div className="w-full navbar-blurred-backdrop max-w-3xl mx-auto flex items-center justify-between">
+          <div className="logo">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src="/logo.svg"
+              alt="Logo"
+              width={24}
+              height={24}
+              className="logo"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
+          <ul className="flex gap-4 items-center tracking-wider font-gochi-hand md:text-lg">
+            <li className="text-stone-400 hover:text-amber-400">
+              <Link href="/about">about</Link>
+            </li>
+            <li className="text-stone-400 hover:text-amber-400">
+              <Link href="https://github.com" target="_blank">
+                github
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      {/* body container */}
+      <main className="flex-grow max-w-3xl mx-auto flex flex-col w-full font-open-sans">
+        <div className="intro my-12 md:my-20">
+          <h2 className="font-gochi-hand text-4xl md:text-6xl text-stone-600 hover:text-stone-400 mb-3 md:mb-5 text-center">
+            Next JumpStart
+          </h2>
+          <p className="text-stone-500 font-medium text-center text-sm md:text-base">
+            An opinionated starter kit for{" "}
+            <Link
+              href={"https://nextjs.org/"}
+              target="_blank"
+              className="hover:text-amber-500"
+            >
+              Nextjs
+            </Link>
+            .
+          </p>
+        </div>
+
+        <div className="contents-of-template flex flex-col p-4 text-sm md:text-base mb-8">
+          <h3 className="font-medium text-stone-500 mb-2">
+            What&apos;s included in the Template?
+          </h3>
+          <ul className="w-full text-stone-400 flex flex-col gap-2">
+            {contentsMap.map((content, index) => (
+              <li key={index} className="">
+                {content.link ? (
+                  <Link
+                    href={content.link}
+                    target="_blank"
+                    className="text-amber-600 hover:text-amber-400"
+                  >
+                    {content.title}
+                  </Link>
+                ) : (
+                  content.title
+                )}
+                : {content.description}
+              </li>
+            ))}
+          </ul>
+
+          <div className="more-info my-6 leading-6 text-sm text-stone-400">
+            For more information related to the template, and recommendations
+            for other libraries for common use-cases, check out the{" "}
+            <Link
+              href={"/about"}
+              target="_blank"
+              className="font-mono text-amber-600 hover:text-amber-400 hover:underline"
+            >
+              /about
+            </Link>{" "}
+            page.
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+
+      {/* footer */}
+      <footer className="flex items-center py-2 border-t border-stone-800 justify-center">
+        <p className="text-stone-500 flex items-center justify-center gap-1 font-gochi-hand tracking-wide md:text-lg">
+          a foundation by{" "}
+          <Link
+            href="https://github.com/akshay-rajput"
+            target="_blank"
+            className="text-amber-500 hover:underline decoration-wavy"
+          >
+            akshay
+          </Link>
+        </p>
       </footer>
     </div>
   );
